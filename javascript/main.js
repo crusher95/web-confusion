3944cdf0b23cb94c57ca1e85866b072db5644637
Utkarsh Dhawan <dhawan.utkarsh@gmail.com>
2015-05-04 22:13:13 +0530

    Design changes

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function sidemenu(){
+	document.getElementById('button-side-menu').style.display="none";
+	document.getElementById('side-menu').style.display="block";
+}
+function closesidemenu(){
+	document.getElementById('button-side-menu').style.display="block";
+	document.getElementById('side-menu').style.display="none";
+}
